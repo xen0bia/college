463b9ad03d675cc1dd9b5453d6c070c83fbd86db
Zynab <zenoobia83@gmail.com>
2021-02-09 19:16:54 -0600

    Added periods to lab3 assignment.

--- csce160/lab3/c3e3.py
+++ csce160/lab3/c3e3.py
@@ -10,13 +10,13 @@ def main():
 
     #Determine life stage based off of the age inputted
     if x <= 1:
-        print('The person is an infant\n')
+        print('The person is an infant.\n')
     if x > 1 and x < 13:
-        print('The person is a child\n')
+        print('The person is a child.\n')
     if 13 <= x < 20:
-        print('The person is a teenager\n')
+        print('The person is a teenager.\n')
     if x >= 20:
-        print('The person is an adult\n')
+        print('The person is an adult.\n')
 
 if __name__ == "__main__":
     main()
