1e4a76c448bc9aff9fc139024fed340eec659e69
Zynab <zenoobia83@gmail.com>
2021-02-24 20:13:23 -0600

    extra exercise

---
+++ csce160/lab4/c4e15.py
@@ -0,0 +1,15 @@
+'''
+Chapter 4
+Zynab Ali
+'''
+
+def main():
+    print()
+    for i in range(6):
+        print('#', end='')
+        print(' ' * i, end='')
+        print('#')
+    print()
+
+if __name__ == '__main__':
+    main()
